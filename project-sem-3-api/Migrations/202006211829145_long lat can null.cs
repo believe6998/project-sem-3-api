d7cc3ea9a65c70211ec59e95ab25d748c8a866c4
Believe <namph@meeyland.com>
2020-06-22 02:04:13 +0700

    dang lam do

---
+++ project-sem-3-api/Migrations/202006211829145_long lat can null.cs
@@ -0,0 +1,22 @@
+﻿namespace project_sem_3_api.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class longlatcannull : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Stations", "Longitudes", c => c.Decimal(precision: 18, scale: 2));
+            AlterColumn("dbo.Stations", "Latitudes", c => c.Decimal(precision: 18, scale: 2));
+            AddColumn("dbo.report", "Status",
+                c => c.Int(nullable: false, defaultValue: 1));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Stations", "Latitudes", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AlterColumn("dbo.Stations", "Longitudes", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+    }
+}
