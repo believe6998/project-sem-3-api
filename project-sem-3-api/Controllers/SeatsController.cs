8825a3bb795a2f6cb5f08e653630de7346c53751
Duong Phu <phudq6@fpt.com.vn>
2020-06-30 11:44:42 +0700

    Some APi

--- project-sem-3-api/Controllers/SeatsController.cs
+++ project-sem-3-api/Controllers/SeatsController.cs
@@ -28,11 +28,13 @@ namespace project_sem_3_api.Controllers
                          join st in db.SeatTypes on tc.IdTrainCarType equals st.IdTrainCarType
                          join s in db.Seats on st.Id equals s.IdSeatType
                          join tk in db.Tickets on new { 
-                             IdSeat = s.Id, 
+                             IdSeat = s.Id,
+                             IdTrainCar = tc.Id,
                              DepartureDay
                          } equals 
                          new { 
                              tk.IdSeat,
+                             IdTrainCar = tk.IdTrainCar,
                              tk.DepartureDay
                          } into tickets
                          from ticket in tickets.DefaultIfEmpty()
@@ -49,7 +51,8 @@ namespace project_sem_3_api.Controllers
                                  endPoint == null || 
                                  startPoint.IndexNumber >= endTs.IndexNumber ||
                                  endPoint.IndexNumber <= startTs.IndexNumber,
-                            s.SeatNo
+                            s.SeatNo,
+                            st.Price
                          };
 
             // Handle case multi route in big route
@@ -59,9 +62,9 @@ namespace project_sem_3_api.Controllers
             {
                 var currentSeat = oldResult[i];
                 var indexSeatExited = newResult.FindIndex(x => x.Id == currentSeat.Id);
-                if (indexSeatExited >= 0 && !currentSeat.EmptySeat)
+                if (indexSeatExited >= 0)
                 {
-                    newResult[indexSeatExited] = currentSeat;
+                    newResult[indexSeatExited] = !currentSeat.EmptySeat ? currentSeat : newResult[indexSeatExited];
                 }
                 else
                 {
--- project-sem-3-api/Controllers/ValuesController.cs
+++ project-sem-3-api/Controllers/ValuesController.cs
@@ -14,60 +14,24 @@ namespace project_sem_3_api.Controllers
         private MyDatabaseContext db = new MyDatabaseContext();
 
         // GET api/values
-        public IEnumerable<dynamic> Get(int StartStation, int EndStation, DateTime? StartDate)
+        public IEnumerable<dynamic> Get()
         {
             // join t in db.Trains on ts.IdTrain equals t.Id
             // join s in db.Stations on ts.IdStation equals s.Id
-            var result = from s in (
-                            from s1 in db.TrainStations.Cast<TrainStation>()
-                            where s1.IdStation == StartStation
-                            select new
-                            {
-                                TrainId = s1.IdTrain,
-                                StartStaion = s1.IdStation,
-                                StartTime = s1.ArrivalTime,
-                                StartIndex = s1.IndexNumber
-                            }
-                        )
-                         join e in (
-                             from e1 in db.TrainStations.Cast<TrainStation>()
-                             where e1.IdStation == EndStation
-                             select new
-                             {
-                                 TrainId = e1.IdTrain,
-                                 IdEndStaion = e1.IdStation,
-                                 EndTime = e1.ArrivalTime,
-                                 EndIndex = e1.IndexNumber
-                             }
-                             ) on s.TrainId equals e.TrainId
-                         join t in db.Trains on s.TrainId equals t.Id
-                         where e.EndTime > s.StartTime
+            var result = from s in db.Stations
                          select new
                          {
-                             TrainId = t.Id,
-                             TrainCode = t.Code,
-                             s.StartStaion,
-                             e.IdEndStaion,
-                             s.StartIndex,
-                             e.EndIndex,
-                             TravelTime = e.EndTime - s.StartTime,
-                             routeDetails = (
-                                 from ts in db.TrainStations
-                                 join st in db.Stations on ts.IdStation equals st.Id
-                                 where ts.IdTrain == t.Id
-                                 && ts.IndexNumber >= s.StartIndex
-                                 && ts.IndexNumber <= e.EndIndex
-                                 select new
-                                 {
-                                     ts.ArrivalTime,
-                                     st.Name,
-                                     st.Location.Latitude,
-                                     st.Location.Longitude,
-                                 }
-                           ).ToList()
+                             s.Id,
+                             s.Name,
+                             //prev = Sql.Ext.Lag(s.Id, Sql.Nulls.None).Over().OrderBy(s.CreatedAt).ToValue()
                          };
 
-            return result;
+            var result2 = from x in db.Stations
+            select new { x.Id, prev = Sql.Ext.Sum(x.Id).Over().ToValue() };
+
+            var a = result2.First();
+
+            return result2;
         }
 
         // GET api/values/5
