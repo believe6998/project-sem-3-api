a7e19a506a7ed83ca608c2ee30f76646f776fee3
Duong Phu <phudq6@fpt.com.vn>
2020-07-13 23:26:07 +0700

    change succes page payment

--- project-sem-3-api/Controllers/PaymentPaypalController.cs
+++ project-sem-3-api/Controllers/PaymentPaypalController.cs
@@ -25,7 +25,7 @@ namespace project_sem_3_api.Controllers
             order.Status = 2;
             db.Entry(order).State = EntityState.Modified;
             db.SaveChanges();
-            return Redirect("Success", order);
+            return View("Success", order);
         }
 
         public ActionResult CheckRedirect()
